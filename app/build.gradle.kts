f28f7c9b1beae27f327206598ede79ab9afb6753
Frdl <fardeelazhar82@gmail.com>
2025-03-24 01:07:53 +0500

    Simple Home Ui is Done Closed #2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.chatify"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.chatify"
@@ -56,4 +56,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // google icons
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
 }
